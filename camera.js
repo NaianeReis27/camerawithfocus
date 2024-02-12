fcd9694af894d45b9204c6c67dd2ada7b8562855
NaianeReis27 <email.annereis@gmail.com>
2024-02-12 15:17:23 -0300

    feat: focus distance stringfy

--- camera.js
+++ camera.js
@@ -1,87 +1,34 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const videoElement = document.getElementById('video');
-    const cameraSelect = document.getElementById('cameraSelect');
-    const infoCam = document.getElementById('infoCam');
-    const infoCam2 = document.getElementById('infoCam2');
+    let cameraSelectId;
 
-    let currentStream; // Variável para armazenar a stream atual
+    let currentStream; 
+    const constraints = {
+        video: true
+    }
+    if (cameraSelectId) {
+        constraints.video.deviceId = cameraSelectId;
+    }
 
     try {
-        const devices = await navigator.mediaDevices.enumerateDevices(ele => console.log(ele));
-        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const tracks = stream.getTracks();
 
-        videoDevices.forEach((device, index) => {
-
-            const option = document.createElement('option');
-            option.value = device.groupId;
-            option.text = `Camera ${index + 1}`;
-            cameraSelect.appendChild(option);
-        });
-
-        cameraSelect.addEventListener('change', async () => {
-            const groupId = cameraSelect.value;
-            const constraints = {
-                video: {
-                    groupId: { exact: groupId }
-                }
-            };
-
-            try {
-                // Parar a stream anterior, se houver
-                if (currentStream) {
-                    const tracks = currentStream.getTracks();
-                    tracks.forEach(track => track.stop());
-                }
-
-                infoCam.innerHTML = ''; // Limpar informações anteriores
-                infoCam2.innerHTML = '';
-                const stream = await navigator.mediaDevices.getUserMedia(constraints);
-                const track = stream.getTracks();
-                const ul = document.createElement('ul');
-                const ul2 = document.createElement('ul');
-
-                track.forEach(ele => {
-                    Object.entries(ele.getSettings()).forEach(([key, value]) => {
-                        const li = document.createElement('li');
-                        const valueString = typeof value === 'object' ? JSON.stringify(value) : value;
-                        li.textContent = `${key}: ${valueString}`;
-                        ul.appendChild(li);
-                    });
-                });
-
-                track.forEach(ele => {
-                    Object.entries(ele.getCapabilities()).forEach(([key, value]) => {
-                        const li = document.createElement('li');
-                        const valueString = typeof value === 'object' ? JSON.stringify(value) : value;
-                        li.textContent = `${key}: ${valueString}`;
-                        ul2.appendChild(li);
-                    });
-                });
-
-                infoCam.append(ul);
-                infoCam2.append(ul2);
-
-                videoElement.srcObject = stream;
-                currentStream = stream; // Atualizar a stream atual
-            } catch (error) {
-                console.error('Error accessing media devices: ', error);
-            }
-        });
-
-        if (videoDevices.length > 0) {
-            const defaultGroupId = videoDevices[0].groupId;
-            const defaultConstraints = {
-                video: {
-                    groupId: { exact: defaultGroupId }
-                }
-            };
-
-            const stream = await navigator.mediaDevices.getUserMedia(defaultConstraints);
-            videoElement.srcObject = stream;
-            currentStream = stream; // Definir a stream atual
-        } else {
-            console.error('No video devices found.');
+        if (currentStream) {
+            const tracks = currentStream.getTracks();
+            tracks.forEach(track => track.stop());
         }
+        const foundTracks = tracks.filter(track => {
+            const capabilities = track.getCapabilities();
+            return capabilities.facingMode === 'environment' && capabilities.focusMode.includes('continuous');
+        });
+        if (foundTracks.length > 0) {
+            cameraSelectId = foundTracks[0].id
+        }
+        console.log(foundTracks)
+
+        videoElement.srcObject = stream;
+        currentStream = stream; 
     } catch (error) {
         console.error('Error accessing media devices: ', error);
     }
