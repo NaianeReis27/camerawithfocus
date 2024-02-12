1c3e0ded7bb4cc572c511dd53c5c02058b04df37
NaianeReis27 <email.annereis@gmail.com>
2024-02-12 15:45:05 -0300

    feat: focus distance stringfy

--- camera.js
+++ camera.js
@@ -1,46 +1,50 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const videoElement = document.getElementById('video');
-    let cameraSelectId;
     let currentStream;
 
-    // Função para acessar a câmera com base nas restrições
     async function accessCamera() {
         const constraints = {
             video: {
                 facingMode: 'environment'
             }
-        }
-        
+        };
+
         try {
             const stream = await navigator.mediaDevices.getUserMedia(constraints);
-            const tracks = stream.getTracks();
 
-            // Parar as faixas da stream anterior, se houver
+            // Parar o stream de vídeo atual, se houver
             if (currentStream) {
-                const currentTracks = currentStream.getTracks();
-                currentTracks.forEach(track => track.stop());
+                stopStream(currentStream);
             }
 
             // Filtrar as faixas de vídeo que correspondem aos critérios desejados
-            const foundTracks = tracks.filter(track => {
+            const foundTracks = stream.getVideoTracks().filter(track => {
                 const capabilities = track.getCapabilities();
                 return capabilities.facingMode === 'environment' && capabilities.focusMode.includes('continuous');
             });
 
-            // Se uma faixa com foco correto for encontrada, atualize cameraSelectId
             if (foundTracks.length > 0) {
-                constraints.video.deviceId = cameraSelectId;
-                const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                // Se uma faixa com foco correto for encontrada, atualize a stream atual
+                currentStream = stream;
+                console.log('Câmera encontrada:', foundTracks[0]);
+            } else {
+                console.warn('Nenhuma câmera encontrada com os critérios especificados.');
             }
 
-            console.log(foundTracks);
+            // Exibir a stream no elemento de vídeo
             videoElement.srcObject = stream;
-            currentStream = stream;
         } catch (error) {
-            console.error('Error accessing media devices: ', error);
+            console.error('Erro ao acessar a câmera:', error);
         }
     }
 
+    // Função para parar um stream de mídia
+    function stopStream(stream) {
+        stream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
 
+    // Chamada inicial para acessar a câmera
     await accessCamera();
 });
