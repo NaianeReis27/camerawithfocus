a58de53367b635c6746a04278e6a3b254c370d0a
NaianeReis27 <email.annereis@gmail.com>
2024-02-12 15:32:59 -0300

    feat: focus distance stringfy

--- camera.js
+++ camera.js
@@ -1,35 +1,48 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const videoElement = document.getElementById('video');
     let cameraSelectId;
-
     let currentStream; 
-    const constraints = {
-        video: true
-    }
-    if (cameraSelectId) {
-        constraints.video.deviceId = cameraSelectId;
-    }
 
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        const tracks = stream.getTracks();
-
-        if (currentStream) {
-            const tracks = currentStream.getTracks();
-            tracks.forEach(track => track.stop());
+    // Função para acessar a câmera com base nas restrições
+    async function accessCamera() {
+        const constraints = {
+            video: {
+                facingMode: 'environment'
+            }
         }
-        const foundTracks = tracks.filter(track => {
-            const capabilities = track.getCapabilities();
-            return capabilities.facingMode === 'environment' && capabilities.focusMode.includes('continuous');
-        });
-        if (foundTracks.length > 0) {
-            cameraSelectId = foundTracks[0].id
+        if (cameraSelectId) {
+            constraints.video.deviceId = cameraSelectId;
         }
-        console.log(foundTracks)
 
-        videoElement.srcObject = stream;
-        currentStream = stream; 
-    } catch (error) {
-        console.error('Error accessing media devices: ', error);
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia(constraints);
+            const tracks = stream.getTracks();
+
+            // Parar as faixas da stream anterior, se houver
+            if (currentStream) {
+                const currentTracks = currentStream.getTracks();
+                currentTracks.forEach(track => track.stop());
+            }
+
+            // Filtrar as faixas de vídeo que correspondem aos critérios desejados
+            const foundTracks = tracks.filter(track => {
+                const capabilities = track.getCapabilities();
+                return capabilities.facingMode === 'environment' && capabilities.focusMode.includes('continuous');
+            });
+
+            // Se uma faixa com foco correto for encontrada, atualize cameraSelectId
+            if (foundTracks.length > 0) {
+                cameraSelectId = foundTracks[0].deviceId;
+            }
+
+            console.log(foundTracks);
+            videoElement.srcObject = stream;
+            currentStream = stream; 
+        } catch (error) {
+            console.error('Error accessing media devices: ', error);
+        }
     }
+
+   
+    await accessCamera();
 });
